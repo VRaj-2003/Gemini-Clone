adad8d1aaaa681671ecfd25e468aa8a7cda9935f
Vaibhav Raj <vaibhavraj6204064029@gmail.com>
2024-10-21 16:15:14 +0530

    Gemini Clone

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './component/Sidebar/Sidebar'
+import Main from './component/Sidebar/Main/main'
+
+
+const App = () => {
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
